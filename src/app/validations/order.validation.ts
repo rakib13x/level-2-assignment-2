959c82954bfa0aa4fb4ece42cac597d2f7c37f20
rakib13x <rakib13x@gmail.com>
2024-05-22 16:44:31 +0600

    OrderValidation   zod Schema  added

---
+++ src/app/validations/order.validation.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+const OrderValidationSchema = z.object({
+  email: z.string().email(),
+  productId: z.string(),
+  price: z.number().positive(),
+  quantity: z.number().positive(),
+});
+
+export default OrderValidationSchema;
